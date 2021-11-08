81635ec30cbd76b0094a0f427c4e524ea29b68de
set01q <set01qq@mail.ru>
2021-11-08 23:43:33 +0300

    add support x64 openssl in scripts

--- js/Qt5.14.2_win_msvc2017_mingw73_32.js
+++ js/Qt5.14.2_win_msvc2017_mingw73_32.js
@@ -200,7 +200,7 @@ function isMySQL(libName)
 
 function isSSL(libName)
 {
-	return baseName(libName).match(/libeay|libssl|ssleay\d*/i) // libeay32.dll  libssl32.dll ssleay32.dll
+	return baseName(libName).match(/libeay|libssl|ssleay|libcrypto\d*/i) // libeay32.dll  libssl32.dll ssleay32.dll libssl-1_1-x64.dll libcrypto-1_1-x64.dll
 }
 
 function isQtCoreLib(libName)
--- js/Qt6.1.2_MSVC2019_64bit_mingw73_32.js
+++ js/Qt6.1.2_MSVC2019_64bit_mingw73_32.js
@@ -205,18 +205,16 @@ function isPostgreSQL(libName)
 	LIBPQ.dll
 	libintl-8.dll
 	libiconv-2.dll
-	libcrypto-1_1-x64.dll
    */
 	libName = baseName(libName);
 	return libName.match(/libpq\d*?/i) ||	
 	       libName.match(/libintl-\d+D?/i) ||	       
-	       libName.match(/libiconv-\d+D?/i) ||
-		   libName.match(/libcrypto-.*?/i)
+	       libName.match(/libiconv-\d+D?/i)
 }
 
 function isSSL(libName)
 {
-	return baseName(libName).match(/libeay|libssl|ssleay\d*/i) // libeay32.dll  libssl32.dll ssleay32.dll
+	return baseName(libName).match(/libeay|libssl|ssleay|libcrypto\d*/i) // libeay32.dll  libssl32.dll ssleay32.dll libssl-1_1-x64.dll libcrypto-1_1-x64.dll
 }
 
 function isQtCoreLib(libName)
